c24e839e7c314f6d99b5579aaf834d16e6457515
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-06-11 09:06:12 +0530

    Update AccountProvider.js

--- chat-app/src/components/account/context/AccountProvider.js
+++ chat-app/src/components/account/context/AccountProvider.js
@@ -14,7 +14,7 @@ export const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(()=>{
-      socket.current = io('https://chat-app-socket-server-twkp.onrender.com')
+      socket.current = io('ws://chat-app-socket-server-twkp.onrender.com')
     },[person])
 
   return (
