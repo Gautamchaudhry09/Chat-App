c71f2d5a2ccd9710b41fc571a7aee7b25879df71
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-06-13 15:39:15 +0530

    Update ChatBox.js

--- chat-app/src/components/chat/chat/ChatBox.js
+++ chat-app/src/components/chat/chat/ChatBox.js
@@ -10,7 +10,7 @@ import { getConversation } from '../../../service/api'
 
 const Component = styled(Box)`
 background-image: url(https://img.freepik.com/free-vector/matrix-style-binary-code-digital-background-with-falling-numbers_1017-25336.jpg?w=900&t=st=1707377360~exp=1707377960~hmac=6441548470f5eb85c9fdd885d46b429ffa28240dec64d7d4446da2b94b10a58c);
-position:relative;
+// position:relative;
 // margin-top:50px;
 height:100vh;
 
